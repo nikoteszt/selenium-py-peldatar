5e682e2e8a262c90dc0a4bf96edaf8208c9d1d7d
Nikolausz László Simon <nikolausz1226@protonmail.com>
2021-06-22 02:09:16 +0200

    Update 003 Feladat: Python modulok gyakorlása

--- selenium-homework/main.py
+++ selenium-homework/main.py
@@ -1,9 +1,8 @@
-from bmodul import x_variable
-
-print(x_variable)
-
-# Alternative solution
-
 import bmodul
 
 print(bmodul.x_variable)
+
+# Alternative solution
+from bmodul import x_variable
+
+print("Alternatív megoldással: ", x_variable)
