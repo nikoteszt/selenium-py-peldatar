79a58ac286f65e7dfead7928ec09b573a586c3c4
Nikolausz László Simon <nikolausz1226@protonmail.com>
2021-06-22 21:27:34 +0200

    Update 016 Feladat: Python Adatok és változók feladat megoldasa

--- python-homework/consumption.py
+++ python-homework/consumption.py
@@ -1,16 +1,16 @@
-orszFogy = int(input("Fogyasztás országúton literben :"))
-varFogy = int(input("Fogyasztás városban literben :"))
-orszUt = int(input("Órszágúti szakasz hossza km-ben :"))
-varUt = int(input("Városi útszakasz hossza km-ben :"))
-benzin = int(input("Benzin ára Ft-ban :"))
+road_consumption = int(input("Fogyasztás országúton literben :"))
+urban_consumption = int(input("Fogyasztás városban literben :"))
+country_road = int(input("Órszágúti szakasz hossza km-ben :"))
+urban_road = int(input("Városi útszakasz hossza km-ben :"))
+petrol = int(input("Benzin ára Ft-ban :"))
 
 # Auto fogyasztása csak oda
-autoFogy = (orszUt * orszFogy + varUt * varFogy) / 100
+auto = (road_consumption * country_road + urban_consumption * urban_road) / 100
 
 # Auto fogyasztása oda-vissza
-autoFogyReturn = 2 * autoFogy
+auto_return = 2 * auto
 
 # A teljes út ára
-szummaKoltseg = benzin * autoFogyReturn
-print("A fogyasztás oda-vissza: ", autoFogyReturn, " liter")
-print("Az utazás költsége: ", szummaKoltseg, " Forint")
+total_cost = petrol * auto_return
+print("A fogyasztás oda-vissza: ", auto_return, " liter")
+print("Az utazás költsége: ", total_cost, " Forint")
