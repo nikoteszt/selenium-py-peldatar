320c615047e695650a5989c436d9ddd6ea06a7f8
Nikolausz László Simon <nikolausz1226@protonmail.com>
2021-07-11 20:36:42 +0200

    006 Feladat: Űrlap automatizálás fájlból megoldas v1.0

---
+++ selenium2-homework/csvfeltoltes.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import csv
+import time
+import filecmp
+
+driver = webdriver.Chrome()
+driver.get("http://localhost:9999/another_form.html")
+
+
+def find_and_clear_by_id(idt):
+    element = driver.find_element_by_id(idt)
+    element.clear()
+    return element
+
+
+add_button = driver.find_element_by_id('submit')
+
+with open('table_in_006.csv', encoding="utf-8") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
+    for row in csvreader:
+        print(row)
+        find_and_clear_by_id("fullname").send_keys(row[0])
+        find_and_clear_by_id("email").send_keys(row[1])
+        find_and_clear_by_id("dob").send_keys(row[2])
+        find_and_clear_by_id("phone").send_keys(row[3])
+        add_button.click()
+
+time.sleep(2)
+export_button = driver.find_element_by_tag_name("button")
+export_button.click()
+time.sleep(2)
+filecmp.clear_cache()
+print(filecmp.cmp('table_in_006.csv', 'C:\\Users\\Simon\\Downloads\\table.csv'))
+
+driver.quit()
